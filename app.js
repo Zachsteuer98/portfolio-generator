608c213197613636fe08f43f013a429128ff25df
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-01-22 17:29:27 -0500

    created my first prompt using inquirer

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage =require('./src/page-template');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      type:'input',
+      name:'name',
+      message: 'what is your name'
+    }
+  ])
+  .then((answers) => console.log(answers))
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage =require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github)
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+//   fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
 
-// const printProfileData = profileDataArr => { 
-//     profileDataArr.forEach(profileItem => console.log(profileItem))
-// }
-
-// printProfileData(profileDataArgs);
