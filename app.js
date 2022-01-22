3f31137dbd3c0c50b065152d02fc3ef0ee23c7e3
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-01-22 18:00:07 -0500

    saving work for feature branch

--- app.js
+++ app.js
@@ -1,15 +1,72 @@
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     /* Pass your questions in here */
     {
       type:'input',
       name:'name',
       message: 'what is your name'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself'
     }
   ])
+}
+
+const promptProject = () => {
+  console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  return inquirer.prompt([
+    {
+      type:'input',
+      name:'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type:'input',
+      name:'description',
+      message: 'Provide a description of the project'
+    },
+    {
+      type:'checkbox',
+      name: ' languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'JQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the Github link to your project (Required)',
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+}
+  promptUser()
   .then((answers) => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
 
 // const fs = require('fs');
 // const generatePage =require('./src/page-template');
